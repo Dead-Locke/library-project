e1275533dcbde14c8dfd366251de31e0fbfc7f37
Dead-Locke <impulse25@ymail.com>
2022-07-27 03:07:55 -0400

    Fixed add book functionality and added counters for each filter. - Fixed an issue that caused newly added books to either ignore choses favorite and read status. - Added functionality to the filter tabs. They now filter the displayed books by the given category/status. - Added a counter for all books and books with the given status in the filter tabs. - Fixed modal form sometimes not dissapearing when user clicked outside of the form field. - Removed an unnecessary function and merged its functionality with a general toggleStatus function. - Minor code cleanup.

--- script.js
+++ script.js
@@ -3,32 +3,23 @@
 var myLibrary = [];
 
 function Book(title, author, pages, read_status, favorite) {
-
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read_status;
     this.favorite = favorite
-
 }
 
 Book.prototype.info = function () {
     return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? ' ✅ Read' : ' ❌ Not read'}`)
 }
 
-function addNew(title, author, pages = 0, read = false, favorite = false) {
-    var newBook = new Book(title, author, pages, read,favorite)
-    myLibrary.push(newBook);
-    console.log(newBook)
-}
-
-
-const hobbit = new Book('The Hobbit', "J.R.R Tolkien", 310, false, true)
+const hobbit = new Book('2001: A Space Odyssey', "Arthur C. Clarke", 221, false, true)
 const lotr = new Book('Lord of the Rings', "J.R.R Tolkien", 1178, false, false)
-const andromeda_strain = new Book('The Andromeda Strain', 'Michael Crichton', 350, true, true)
+const andromeda = new Book('The Andromeda Strain', 'Michael Crichton', 350, true, false)
 const dune = new Book('Dune', "Frank Herbert", 412, true, true)
 
-myLibrary.push(hobbit, lotr, andromeda_strain, dune)
+myLibrary.push(hobbit, lotr, andromeda, dune) // push initial set of books to library
 
 // ---------functions and listeners----------------
 const addNewButton = document.getElementById('addNewButton');
@@ -39,87 +30,115 @@ var deleteButtons = document.querySelectorAll('.delete');
 const modal = document.getElementById('modal')
 const newBookForm = document.getElementById('addBookForm');
 const submitBtn = document.querySelector('.btn');
-
-
-
-
+const allFilter = document.getElementById('all_filter')
+const readFilter = document.getElementById('read_filter')
+const unreadFilter = document.getElementById('unread_filter')
+const favFilter = document.getElementById('fav_filter')
+const allCount = document.querySelector('.allCount')
+const readCount = document.querySelector('.readCount')
+const unreadCount = document.querySelector('.unreadCount')
+const favCount = document.querySelector('.favCount')
 
 function displayBooks(array) {
-    const template = document.getElementById('book_template');
-    while(collection.firstChild) collection.firstChild.remove()
+    const template = document.getElementById('book_template'); //get book template
+    while(collection.firstChild) collection.firstChild.remove() //clear area, also 'collection' is not defined? WTF???
 
-    array.forEach(v => {
-        var clone = template.content.cloneNode(true);
-        clone.querySelector('.book').dataset.index = array.indexOf(v);
+    array.forEach(bk => {
+        var clone = template.content.cloneNode(true); //clone template for book display
+        var t = clone.querySelector('.title'); 
+        var a = clone.querySelector('.author'); 
+        var n = clone.querySelector('.num') 
 
-        if (v.read) clone.querySelector('.read_status').classList.add('read')
-        if (v.favorite) clone.querySelector('.favorite').classList.add('fav')
-        var t = clone.querySelector('.title');
-        t.innerHTML = v.title;
-        var a = clone.querySelector('.author');
-        a.innerHTML = v.author;
-        var n = clone.querySelector('.num')
-        n.innerHTML = v.pages;
-        collection.appendChild(clone) //'collection' is not defined? WTF???
+        clone.querySelector('.book').dataset.index = array.indexOf(bk); //assign index to book from library array
+
+        if (bk.read) clone.querySelector('.read_status').classList.add('read') //toggle read status
+        if (bk.favorite) clone.querySelector('.favorite').classList.add('fav') //toggle fav status
+        t.innerHTML = bk.title; 
+        a.innerHTML = bk.author;
+        n.innerHTML = bk.pages;
+
+        collection.appendChild(clone) //add cloned template to collection on page
+
     })
     
-    initialize(); //update button nodelists and event listeners
+    initializeBookButtons(); //update button nodelists and event listeners
+    updateCount();
+}
+
+function addNew(title, author, pages = 0, read = false, favorite = false) {
+    var newBook = new Book(title, author, pages, read,favorite)
+    myLibrary.push(newBook);
 }
 
 
-function toggleFav(element) {
-    console.log('toggle favorite')
-    let bookParent = element.closest('.book');
-    let idx = bookParent.dataset.index;
-    myLibrary[idx].favorite = !myLibrary[idx].favorite
-    element.classList.toggle('fav')
-}
-
-function toggleReadStatus(element) {
-    let bookParent = element.closest('.book');
-    let idx = bookParent.dataset.index;
-    myLibrary[idx].read = !myLibrary[idx].read;
-    element.classList.toggle('read')
+function toggleStatus(element, status) {
+    let bookParent = element.closest('.book'); //identify closes book to clicked element
+    let idx = bookParent.dataset.index; //get its index value
+    if(status =='read') myLibrary[idx].read = !myLibrary[idx].read; //toggle read stats
+    if(status =='fav') myLibrary[idx].favorite = !myLibrary[idx].favorite; //toggle fav status
+    element.classList.toggle(`${status}`)
+    updateCount();
 }
 
 function deleteBook(element) {
     let bookParent = element.closest('.book');
     let idx = bookParent.dataset.index;
-    let b = myLibrary[idx];
     myLibrary.splice(idx, 1);
     displayBooks(myLibrary)
 }
 
-function initialize(){
+function initializeBookButtons(){
     favButtons = document.querySelectorAll('.favorite');
     readButtons = document.querySelectorAll('.read_status');
     deleteButtons = document.querySelectorAll('.delete');
     
-    favButtons.forEach(x => x.onclick = () => toggleFav(x))
-    readButtons.forEach(x => x.onclick = () => toggleReadStatus(x))
+    favButtons.forEach(x => x.onclick = () => toggleStatus(x,'fav'))
+    readButtons.forEach(x => x.onclick = () => toggleStatus(x,'read'))
     deleteButtons.forEach(x => x.onclick = () => deleteBook(x)) 
+}
+
+function filterBook(status) {
+    if(status == 'r') displayBooks(myLibrary.filter(x=> x.read))
+    if(status == 'f') displayBooks(myLibrary.filter(x=> x.favorite))
+    if(status == 'u') displayBooks(myLibrary.filter(x=> !x.read))
+    if(status == 'a') displayBooks(myLibrary)
+}
+
+function updateCount(){
+    let booksRead = myLibrary.filter(x => x.read)
+    let booksUnread = myLibrary.length - booksRead.length;
+    let booksFav = myLibrary.filter(x=> x.favorite)
+    let allBooks = myLibrary.length
+
+    allCount.innerText = `(${allBooks})`
+    readCount.innerText = `(${booksRead.length})`
+    unreadCount.innerText = `(${booksUnread})`
+    favCount.innerText = `(${booksFav.length})`
 }
 
 closeModal = () => modal.style.display = "none";
 
 newBookForm.onsubmit = (e) => {
     e.preventDefault();
-    let title = newBookForm.elements['titleField']
-    let author = newBookForm.elements['authorField']
-    let pages = newBookForm.elements['pageField']
-    let read = newBookForm.elements['f']
-    let fav = newBookForm.elements['r']
-    console.log(title.value, author.value, pages.value, read.value, fav.value)
-    addNew(title.value, author.value, pages.value, read.value, fav.value);
+    let title = newBookForm.elements['titleField'].value
+    let author = newBookForm.elements['authorField'].value
+    let pages = newBookForm.elements['pageField'].value || 0;
+    let read = document.getElementById('r').checked
+    let fav = document.getElementById('f').checked
+
+    addNew(title, author, pages, read, fav);
     displayBooks(myLibrary);
     closeModal()
-    addBookForm.reset();
+    addBookForm.reset(); //clear form
 }
 
 
-addNewButton.onclick = () => modal.style.display = 'flex'
-window.onclick = (e) => {if(e.target == modal) closeModal()}
+addNewButton.onclick = () => modal.style.display = 'flex' //show form modal
+window.onclick = (e) => {if(e.target == modal) closeModal()} //close form if clicked anywhere but form
+allFilter.onclick = () => filterBook('a');
+readFilter.onclick = () => filterBook('r');
+unreadFilter.onclick = () => filterBook('u');
+favFilter.onclick = () => filterBook('f');
     
-displayBooks(myLibrary)
-
+displayBooks(myLibrary) //initial display of library
 
